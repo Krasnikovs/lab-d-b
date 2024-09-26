1767b3e02a76452bec13dcab00644620fcc08465
ubuntuchair <ralis3333@gmail.com>
2024-09-26 10:02:00 +0300

    B progress

--- B4.cpp
+++ B4.cpp
@@ -8,25 +8,81 @@ Skaitļa dalīšana ciparos jāveic skaitliski.
 #include<iostream>
 using namespace std;
 
+bool nat_num_validate(int num) {
+	if (num <= 0) {
+		cout << "Skaitlis nav naturals" << endl;
+
+		return false;
+	} else {
+		return true;
+	}
+}
+
+void posible_len(int num) {
+	int confirmed_same_num[10] = {};
+	int cycle = 0;
+
+	while (true) {
+		for (int i = 0; i <= cycle; i++) {
+			if (cycle > 0 && confirmed_same_num[cycle] == confirmed_same_num[i]) {
+				continue;
+			} else {
+				confirmed_same_num[cycle] = num % 10;
+			}
+		}
+		num = num / 10;
+
+		// cout << cycle << endl;
+		cout << confirmed_same_num[cycle] << endl;
+		
+		if (num == 0) {
+			break;
+		}
+		cycle +=1;
+	}
+
+	for (int i = 0; i < cycle; i++) {
+		confirmed_same_num[i];
+	}
+
+
+	// cout << sizeof(confirmed_same_num);
+	// while (true) {
+	// if (num % 100 == confirmed_same_num[]) {
+	// 	cout << "whooo" << endl;
+	// }
+	// }
+	
+	// return 0;
+}
+
 int main() {
 	bool continue_prog = true; // nosaka, lai programma izpildītos vismaz vienu reizi
 
 	do {
-		int x;
+		bool nat_num = false;
+		int num;
 
-		cout << "Ievadiet naturalu skaitli: ";
-		cin >> x;
+		while (nat_num == false) {
+			cout << "Ievadiet naturalu skaitli: ";
+			cin >> num;
 
-		int confirmed_deviders[x] = {};
-		int confirmed_times[x] = {};
-
-		for (int devider = 1; devider < x; devider++) {
-			if (x% devider == 0 && devider != 1) {
-				cout << "Atrasts vienāds cipars " << devider << " un cipars ir satopams " << x / devider << ". reizes" << endl;
-				// confirmed_times[devider - 1] = x / devider;
-				// confirmed_deviders[devider - 1] = devider;
-			}
+			nat_num = nat_num_validate(num);
 		}
+
+		// cout << 1;
+
+		posible_len(num);
+		
+		// int confirmed_times[x] = {};
+
+		// for (int devider = 1; devider < num; devider++) {
+		// 	if (num % devider == 0 && devider != 1) {
+		// 		cout << "Atrasts vienāds cipars " << devider << " un cipars ir satopams " << num / devider << ". reizes" << endl;
+		// 		// confirmed_times[devider - 1] = x / devider;
+		// 		// confirmed_deviders[devider - 1] = devider;
+		// 	}
+		// }
 
 		char user_choise;
         std::cout << "Vēlaties turpināt? (Y/n):"; 
@@ -48,12 +104,7 @@ Testa plāns
 Ievads		Programma vēlamā		Rezultāts C++
 				reakcija
 
- 64				 2 32					+
-				 4 16
-				 8	8
-				16	4
-				32	2
-
- 8				 2	4					+
-				 4	2
+ 64					0					+
+				 
+ 818				2					+
 */
